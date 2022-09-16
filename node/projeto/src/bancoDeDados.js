a4efd9c3c550b741375c3a907d66c43ca23639c3
Neto Branco <neto.branco2804@hotmail.com>
2022-09-15 23:20:00 -0300

    Inserindo bancoDeDados.js

---
+++ node/projeto/src/bancoDeDados.js
@@ -0,0 +1,31 @@
+//criar um objeto para representar uma sequencia para que possa pegar os id dos objetos
+
+const sequence ={
+    _id: 1,
+    get id() {return this._id++}
+}
+
+const produtos = {}
+
+
+/*! = Se o id do produto não estiver setado, vou fazer produto.id recebe = sequence.id
+na linha 15, se o produto estiver setado ele substitui pela versão mais atual, caso nao estiver setado ele add um novo id */
+function salvarProduto(produto){
+    if (!produto.id) produto.id = sequence.id
+    produtos[produtos.id] = produto
+    return produto //aqui ja vai retornar produto com id setado
+}
+
+//criar função para pegar produto por id
+function getProduto(id) {
+        return produtos[id] || {} //caso seja nulo ele retornara um objeto vazio
+}
+
+//criar função para retornar todos os produtos
+function getProdutos() {
+    return Object.values(...produtos)
+}
+
+//Como tornar essas funções que estão visiveis apenas dentro desse modulo, deixando visivel para fora
+
+module.exports = {salvarProduto, getProduto, getProdutos}
--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -1,11 +1,30 @@
-const porta = 3003
+const porta = 3009
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
+const bodyParser = require('body-parser') //importo para que o programa postMan funcione
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45}) // converter para JSON
+    res.send(bancoDeDados.getProdutos())
 })
+
+//Trazedo um produto especifico baseado no id, passando um paramentro (:id)
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+//Criar um post para submeter os dados e salvar um novo produto
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({ //salvar produto no pseudo banco de dados
+        nome: req.body.nome, // pegando nome apartir do corpo da requisiçao
+        preco: req.body.preco
+    })
+    res.send(produto) //Retornar produto como resposta= CONVERTEU PRA JSON LEVANDO PRA INTERNET 
+})
+
 
 app.listen(porta, () => {
     console.log(`Servidor executando na porta ${porta}.`)
